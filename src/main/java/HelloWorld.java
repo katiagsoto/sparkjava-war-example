a3f43662ea8bf188a655f2c4f0ad53d0b98fdd15
katiagsoto <114024122+katiagsoto@users.noreply.github.com>
2022-09-27 10:34:39 -0500

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "hola frens");
+		get("/hello", (req, res) -> "Que rollo morros?");
 	}
 }
